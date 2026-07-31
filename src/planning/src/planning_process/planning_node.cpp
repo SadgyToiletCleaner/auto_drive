4fdc65ce69d85ff96a301835db35f8ac1e618e99
zhengshaojie <example@email.com>
2026-07-31 17:45:33 +0800

    规划节点代码编写启动

--- src/planning/src/planning_process/planning_node.cpp
+++ src/planning/src/planning_process/planning_node.cpp
@@ -1,10 +1,38 @@
-#include <cstdio>
+#ifndef PLANNING_PROCESS_H_
+#define PLANNING_PROCESS_H_
 
-int main(int argc, char ** argv)
+#include "rclcpp/rclcpp.hpp"
+
+namespace Planning
 {
-  (void) argc;
-  (void) argv;
+class PlanningProcess : public rclcpp::Node
+  {
+    public:
+      PlanningProcess() : Node("planning_node")
+      {
 
-  printf("hello world planning package\n");
+      }
+      
+      bool process() {return true;} //总流程
+  private:
+  };
+}  // namespace Planning
+#endif  // PLANNING_PROCESS_H_
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  RCLCPP_INFO(rclcpp::get_logger("planning_node"), "planning start");
+
+  auto node = std::make_shared<Planning::PlanningProcess>();
+  if(!node->process())
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("planning_node"), "planning failed!");
+    rclcpp::shutdown();
+    return 1;
+  }
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
