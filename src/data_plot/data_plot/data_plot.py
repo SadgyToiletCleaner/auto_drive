24916be9ae41792bb7be1ce489d6a6d2f82ea793
zhengshaojie <example@email.com>
2026-07-31 15:48:25 +0800

    planning目录结构改造

--- src/data_plot/data_plot/data_plot.py
+++ src/data_plot/data_plot/data_plot.py
@@ -1,5 +1,23 @@
+import rclpy
+import numpy as np
+import matplotlib.pyplot as plt
+from rclpy.node import Node
+
+class PlotData(Node):
+    def __init__(self):
+        super().__init__("data_plot_node")
+        self.get_logger().info("data_plot_node created")
+
 def main():
-    print('Hi from data_plot.')
+    rclpy.init()
+    plot_node = PlotData()
+
+    try:
+        rclpy.spin(plot_node)
+    except KeyboardInterrupt:
+        print("用户关闭")
+    finally:
+        rclpy.shutdown() # 防止按ctrl + c时无法关闭
 
 
 if __name__ == '__main__':
